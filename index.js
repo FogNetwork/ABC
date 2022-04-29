d062fdb06564997ccc9fa351d85f078df4d6064d
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2022-04-28 17:58:01 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+class Windowo {
+constructor(config = {}) {
+this.type = config.type || "blank"
+this.url = config.url || "about:blank"
+}
+setType(type) {
+if (!type) return;
+this.type = type
+}
+setUrl(url) {
+if (!url) return;
+this.url = url
+}
+getCode() {
+return `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + this.url + `"></iframe>`
+}
+open() {
+if (this.type == "blank") {
+var page = window.open()
+page.document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + this.url + `"></iframe>`
+} else if (this.type == "blob") {
+var page = new Blob([`<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + this.url + `"></iframe>`], {type: "text/html"})
+window.open(URL.createObjectURL(page))
+} else if (this.type == "overwrite") {
+document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + this.url + `"></iframe>`
+}
+}
+}
