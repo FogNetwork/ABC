6a3f089db4f291c71655ebe58bd032ce015e86b6
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2022-04-28 18:13:22 -0700

    fix errors

--- index.js
+++ index.js
@@ -16,13 +16,22 @@ return `<iframe style="height:100%; width: 100%; border: none; position: fixed; 
 }
 open() {
 if (this.type == "blank") {
+try {
 var page = window.open()
 page.document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + this.url + `"></iframe>`
+} catch {
+}
 } else if (this.type == "blob") {
+try {
 var page = new Blob([`<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + this.url + `"></iframe>`], {type: "text/html"})
 window.open(URL.createObjectURL(page))
+} catch {
+}
 } else if (this.type == "overwrite") {
+try {
 document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + this.url + `"></iframe>`
+} catch {
+}
 }
 }
 }
